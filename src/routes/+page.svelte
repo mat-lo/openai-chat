777d7d47e7fe3b4ed3be05f6ff168dcb6c26f31e
matlo <write@matlo.me>
2023-03-19 10:55:04 +0000

    first

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '../app.css';
+
+	let prompt = '';
+	let output = '';
+
+	const generateChatCompletion = async () => {
+		const response = await fetch('/api/chatCompletion', {
+			method: 'POST',
+			body: JSON.stringify({ prompt: prompt }),
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+
+		const { result } = await response.json();
+
+		// typing effect!
+		let i = 0;
+		let typingEffect = setInterval(() => {
+			output += result[i];
+			i++;
+			if (i > result.length - 1) {
+				clearInterval(typingEffect);
+			}
+		}, 20);
+	};
+</script>
+
+<input type="text" bind:value={prompt} placeholder="input yo!" />
+<button on:click={generateChatCompletion}>Calculate</button>
+<p>{output}</p>
---
+++ src/routes/api/chatCompletion/+server.js
@@ -0,0 +1,40 @@
+import { error, json } from '@sveltejs/kit';
+import { OPENAI_API_KEY } from '$env/static/private';
+import { Configuration, OpenAIApi } from 'openai';
+const systemPrompt = 'You are Roby, the AI creative director.';
+
+const configuration = new Configuration({
+	apiKey: OPENAI_API_KEY
+});
+
+const openai = new OpenAIApi(configuration);
+
+export async function POST({ request }) {
+	if (!configuration.apiKey) {
+		throw error(500, 'OpenAI API key not configured, please follow instructions in README.md');
+	}
+
+	const { prompt } = await request.json();
+
+	if (prompt.trim().length === 0) {
+		throw error(400, 'Please enter a valid prompt');
+	}
+
+	try {
+		const completion = await openai.createChatCompletion({
+			model: 'gpt-3.5-turbo',
+			messages: [
+				{ role: 'system', content: systemPrompt },
+				{ role: 'user', content: prompt }
+			]
+		});
+		return json({ result: completion.data.choices[0].message.content });
+	} catch (error) {
+		// Consider adjusting the error handling logic for your use case
+		if (error.response) {
+			throw error(error.response.status, error.response.data);
+		} else {
+			throw error(500, `Error with OpenAI API request: ${error.message}`);
+		}
+	}
+}
